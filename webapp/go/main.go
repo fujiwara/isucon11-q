864a90cdca61ff3e8fb76ebd65ccf57387371ebf
mackee <macopy123@gmail.com>
2021-08-21 18:05:36 +0900

    isu_conditionを複合PKにする

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -83,7 +83,6 @@ type GetIsuListResponse struct {
 }
 
 type IsuCondition struct {
-	ID                 int       `db:"id"`
 	JIAIsuUUID         string    `db:"jia_isu_uuid"`
 	Timestamp          time.Time `db:"timestamp"`
 	IsSitting          bool      `db:"is_sitting"`
