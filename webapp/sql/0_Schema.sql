490c7322a6fdba0d1b31645c00efd4f17a357242
mackee <macopy123@gmail.com>
2021-08-21 10:52:50 +0900

    isu_conditionのorder by狙い

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -35,3 +35,5 @@ CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
   `url` VARCHAR(255) NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+ALTER TABLE `isu_condition` ADD INDEX `jia_isu_uuid_timestamp_idx` (`jia_isu_uuid`, `timestamp`);
